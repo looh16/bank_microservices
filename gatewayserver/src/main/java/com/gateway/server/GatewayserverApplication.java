50eb530ae157ac21b12205f4d3db9203181eca70
looh16 <langacustodio@gmail.com>
2023-05-04 12:07:30 +0200

    Add customer routes

--- gatewayserver/src/main/java/com/gateway/server/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/gateway/server/GatewayserverApplication.java
@@ -1,8 +1,13 @@
 package com.gateway.server;
 
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +17,23 @@ public class GatewayserverApplication {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+	    return builder.routes()
+	        .route(p -> p
+	            .path("/bank/accounts/**")
+	            .filters(f -> f.rewritePath("/bank/accounts/(?<segment>.*)","/${segment}")
+	            				.addResponseHeader("X-Response-Time",new Date().toString()))
+	            .uri("lb://ACCOUNTS")).
+	        route(p -> p
+		            .path("/bank/loans/**")
+		            .filters(f -> f.rewritePath("/bank/loans/(?<segment>.*)","/${segment}")
+		            		.addResponseHeader("X-Response-Time",new Date().toString()))
+		            .uri("lb://LOANS")).
+	        route(p -> p
+		            .path("/bank/cards/**")
+		            .filters(f -> f.rewritePath("/bank/cards/(?<segment>.*)","/${segment}")
+		            		.addResponseHeader("X-Response-Time",new Date().toString()))
+		            .uri("lb://CARDS")).build();
+	}
 }
