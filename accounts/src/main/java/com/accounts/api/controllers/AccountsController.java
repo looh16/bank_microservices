a8d2801e84cccea1c92ce8f5ddeeff63d1441232
looh16 <langacustodio@gmail.com>
2023-05-05 12:20:35 +0200

    Add zipkin configurations

--- accounts/src/main/java/com/accounts/api/controllers/AccountsController.java
+++ accounts/src/main/java/com/accounts/api/controllers/AccountsController.java
@@ -2,6 +2,8 @@ package com.accounts.api.controllers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,6 +37,8 @@ import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 
 @RestController
 public class AccountsController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(AccountsController.class);
 	
 	@Autowired
 	private AccountsRepository accountsRepository;
@@ -72,6 +76,7 @@ public class AccountsController {
 	@PostMapping("/myCustomerDetails")
 	@CircuitBreaker(name = "detailsForCustomerSupportApp",fallbackMethod="myCustomerDetailsFallBack")
 	public CustomerDetails myCustomerDetails(@RequestHeader("bank-correlation-id") String correlationid,@RequestBody Customer customer) {
+		logger.info("myCustomerDetails() method started");
 		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
 		List<Loans> loans = loansClient.getLoansDetails(correlationid,customer);
 		List<Cards> cards = cardsClient.getCardDetails(correlationid,customer);
@@ -80,6 +85,7 @@ public class AccountsController {
 		customerDetails.setAccounts(accounts);
 		customerDetails.setLoans(loans);
 		customerDetails.setCards(cards);
+		logger.info("myCustomerDetails() method ended");
 		
 		return customerDetails;
 
--- loans/src/main/java/com/loans/api/controllers/LoansController.java
+++ loans/src/main/java/com/loans/api/controllers/LoansController.java
@@ -2,6 +2,8 @@ package com.loans.api.controllers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +29,8 @@ import com.loans.api.repository.LoansRepository;
 
 @RestController
 public class LoansController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(LoansController.class);
 
 	@Autowired
 	private LoansRepository loansRepository;
@@ -36,8 +40,10 @@ public class LoansController {
 
 	@PostMapping("/myLoans")
 	public List<Loans> getLoansDetails(@RequestHeader("bank-correlation-id") String correlationid,@RequestBody Customer customer) {
+		logger.info("getLoansDetails() method started");
 		System.out.println("Invoking Loans Microservice");
 		List<Loans> loans = loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
+		logger.info("getLoansDetails() method ended");
 		if (loans != null) {
 			return loans;
 		} else {
