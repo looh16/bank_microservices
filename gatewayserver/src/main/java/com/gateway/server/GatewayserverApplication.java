e486850551f349a2716e01aac2591c4232ff83de
looh16 <langacustodio@gmail.com>
2023-05-12 21:10:07 +0200

    Add configurations to secure microservices and update helm charts

--- gatewayserver/src/main/java/com/gateway/server/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/gateway/server/GatewayserverApplication.java
@@ -2,38 +2,65 @@ package com.gateway.server;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import io.micrometer.context.ContextSnapshot;
+import io.micrometer.tracing.Tracer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.factory.TokenRelayGatewayFilterFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+import com.gateway.server.trace.ObservationContextSnapshotLifter;
+
+import reactor.core.publisher.Hooks;
+import reactor.core.publisher.Operators;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 
+	@Autowired
+	private TokenRelayGatewayFilterFactory filterFactory;
+
 	@Bean
 	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
 	    return builder.routes()
 	        .route(p -> p
 	            .path("/bank/accounts/**")
-	            .filters(f -> f.rewritePath("/bank/accounts/(?<segment>.*)","/${segment}")
-	            				.addResponseHeader("X-Response-Time",new Date().toString()))
+	            .filters(f -> f.filters(filterFactory.apply())
+					.rewritePath("/bank/accounts/(?<segment>.*)","/${segment}")
+					.removeRequestHeader("Cookie"))
 	            .uri("lb://ACCOUNTS")).
 	        route(p -> p
 		            .path("/bank/loans/**")
-		            .filters(f -> f.rewritePath("/bank/loans/(?<segment>.*)","/${segment}")
-		            		.addResponseHeader("X-Response-Time",new Date().toString()))
+					.filters(f -> f.filters(filterFactory.apply())
+							.rewritePath("/bank/loans/(?<segment>.*)","/${segment}")
+							.removeRequestHeader("Cookie"))
 		            .uri("lb://LOANS")).
 	        route(p -> p
 		            .path("/bank/cards/**")
-		            .filters(f -> f.rewritePath("/bank/cards/(?<segment>.*)","/${segment}")
-		            		.addResponseHeader("X-Response-Time",new Date().toString()))
+					.filters(f -> f.filters(filterFactory.apply())
+							.rewritePath("/bank/cards/(?<segment>.*)","/${segment}")
+							.removeRequestHeader("Cookie"))
 		            .uri("lb://CARDS")).build();
 	}
+
+	@ConditionalOnClass({ContextSnapshot.class, Hooks.class})
+	@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
+	@Bean
+	public ApplicationListener<ContextRefreshedEvent> reactiveObservableHook() {
+		return event -> Hooks.onEachOperator(
+				ObservationContextSnapshotLifter.class.getSimpleName(),
+				Operators.lift(ObservationContextSnapshotLifter.lifter()));
+	}
+
 }
